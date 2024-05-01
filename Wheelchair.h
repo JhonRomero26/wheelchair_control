6d3d35e6211335b0dae4271e8ea05fa4084a0e12
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2024-05-01 09:19:51 -0500

    feat: set attributes for speed engines and speed limiter

--- Wheelchair.h
+++ Wheelchair.h
@@ -6,7 +6,10 @@
 class Wheelchair {
   private:
     BleController *ble;
-  
+    int leftEngineSpeed = 0;
+    int rightEngineSpeed = 0;
+    float speedPercent = 0.15;
+
   public:
     Wheelchair(BleController *ble);
     void begin(int bauds);
