035370bc30873d988b849149d848d4616addba3c
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2024-05-04 14:01:03 -0500

    refactor: move wheelchair definitions to header

--- Wheelchair.cpp
+++ Wheelchair.cpp
@@ -3,16 +3,6 @@
 #include "SabertoothSimplified.h"
 
 
-#define MAX_SPEED_MOTOR 127  // max speed motor
-
-#ifndef TIME_FOR_MOVE
-  #define TIME_FOR_MOVE  180   // time for move in ms
-#endif
-
-#ifndef TIME_FOR_STOP
-  #define TIME_FOR_STOP  50   // time for move in ms
-#endif
-
 SabertoothSimplified st;
 int oldTime = 0;
 char directionCommand[32];
@@ -22,35 +12,6 @@ enum MOTORS {
   MOTOR_RIGHT = 2
 };
 
-// MOVEMENT_COMMAND
-#define WHEELCHAIR_MOVE_FORWARD "mf\0\n"
-#define WHEELCHAIR_MOVE_LEFT "ml\0\n"
-#define WHEELCHAIR_MOVE_LEFT2 "ml2\0\n"
-#define WHEELCHAIR_MOVE_LEFT3 "ml3\0\n"
-#define WHEELCHAIR_MOVE_BACKWARD "mb\0\n"
-#define WHEELCHAIR_MOVE_RIGHT "mr\0\n"
-#define WHEELCHAIR_MOVE_RIGHT2 "mr2\0\n"
-#define WHEELCHAIR_MOVE_RIGHT3 "mr3\0\n"
-#define WHEELCHAIR_MOVE_STOP "ms\0\n"
-#define WHEELCHAIR_ROTATE_LEFT "rl\0\n"
-#define WHEELCHAIR_ROTATE_RIGHT "rr\0\n"
-#define WHEELCHAIR_ROTATE_LEFT_VOICE "rlv\0\n"
-#define WHEELCHAIR_ROTATE_RIGHT_VOICE "rrv\0\n"
-
-// SPEED_COMMAND
-#define WHEELCHAIR_SPEED_SLOW "sv\0\n"
-#define WHEELCHAIR_SPEED_MEDIUM "nv\0\n"
-#define WHEELCHAIR_SPEED_FAST "fv\0\n"
-
-#define WHEELCHAIR_SPEED_SLOW_VAlUE 0.2
-#define WHEELCHAIR_SPEED_MEDIUM_VAlUE 0.4
-#define WHEELCHAIR_SPEED_FAST_VAlUE 0.5
-
-#define WHEELCHAIR_MOVE_LEFT_RIGHT_PERCENT 0.15
-#define WHEELCHAIR_MOVE_LEFT_RIGHT_PERCENT2 0.25
-#define WHEELCHAIR_MOVE_LEFT_RIGHT_PERCENT3 0.35
-
-#define WHEELCHAIR_ENGINE_CORRECTION_PERCENT 0.85
 
 Wheelchair::Wheelchair(BleController *ble) {
   this->ble = ble;
