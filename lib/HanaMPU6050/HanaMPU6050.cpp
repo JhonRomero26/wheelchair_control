023306978bad791910353dcaeb639fe76d32177c
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2024-05-18 18:25:10 -0500

    fix: kalman calculate delay measurement fixed

--- lib/HanaMPU6050/HanaMPU6050.cpp
+++ lib/HanaMPU6050/HanaMPU6050.cpp
@@ -1,71 +1,133 @@
 #include "HanaMPU6050.h"
 
 
-void HanaMPU6050::begin() {
-  Wire.setClock(HANA_MPU6050_CLOCK);
+float rateRoll, ratePitch, rateYaw;
+float rateCalibrationRoll, rateCalibrationPitch, rateCalibrationYaw;
+int rateCalibrationNumber;
+
+float accX, accY, accZ;
+float angleRoll, anglePitch, angleYaw;
+
+float kalmanAngleRoll = 0,
+  kalmanUncertaintyAngleRoll = 2*2;
+float kalmanAnglePitch = 0,
+  kalmanUncertaintyAnglePitch = 2*2;
+
+float kalman1DOutput[] = {0, 0};
+float rad2deg = 180 / PI;
+
+float gyro_meassurement = MPU6050_MEASUREMENT_250_DEG;
+int accel_measurement = MPU6050_MEASUREMENT_2_G;
+
+
+void HanaMPU6050::setClock(int clock) {
+  Wire.setClock(clock);
+}
+
+void HanaMPU6050::setAccelerometerRange(mpu6050_accel_range_t range) {
+  accel_range = range;
+
+  if (range == MPU6050_RANGE_16_G) {
+    accel_measurement = MPU6050_MEASUREMENT_16_G;
+  } else if (range == MPU6050_RANGE_4_G) {
+    accel_measurement = MPU6050_MEASUREMENT_4_G;
+  } else if (range == MPU6050_RANGE_8_G) {
+    accel_measurement = MPU6050_MEASUREMENT_8_G;
+  }
+}
+
+void HanaMPU6050::setGyroRange(mpu6050_gyro_range_t gyro) {
+  gyro_range = gyro;
+  
+  if (gyro == MPU6050_RANGE_500_DEG) {
+    gyro_meassurement = MPU6050_MEASUREMENT_500_DEG;
+  } else if (gyro == MPU6050_RANGE_1000_DEG) {
+    gyro_meassurement = MPU6050_MEASUREMENT_1000_DEG;
+  } else if (gyro == MPU6050_RANGE_2000_DEG) {
+    gyro_meassurement = MPU6050_MEASUREMENT_2000_DEG;
+  }
+}
+
+void HanaMPU6050::getAngle(mpu6050_angle_t *angle) {
+  angle->roll = kalmanAngleRoll;
+  angle->pitch = kalmanAnglePitch;
+}
+
+void HanaMPU6050::getAcceleration(mpu6050_accel_t *acc) {
+  acc->x = accX;
+  acc->y = accY;
+  acc->z = accZ;
+}
+
+void HanaMPU6050::getRate(mpu6050_rate_t *rate) {
+  rate->roll = rateRoll;
+  rate->pitch = ratePitch;
+  rate->yaw = rateYaw;
+}
+
+void HanaMPU6050::getGyro(mpu6050_gyro_t *gyro) {
+  gyro->roll = rateRoll;
+  gyro->pitch = ratePitch;
+  gyro->yaw = rateYaw;
+}
+
+void HanaMPU6050::begin(double *ts, uint8_t i2c_addr) {
+  i2c_address = i2c_addr;
+  this->Ts = ts;
+  
   Wire.begin();
   delay(250);
-  Wire.beginTransmission(0x68);
+  
+  Wire.beginTransmission(i2c_address);
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
 
   for (
-    _rateCalibrationNumber = 0;
-    _rateCalibrationNumber < 2000;
-    _rateCalibrationNumber++
+    rateCalibrationNumber = 0;
+    rateCalibrationNumber < 2000;
+    rateCalibrationNumber++
   ) {
     mpuSignals();
-    _rateCalibrationRoll += rateRoll;
-    _rateCalibrationPitch += ratePitch;
-    _rateCalibrationYaw += rateYaw;
+    rateCalibrationRoll += rateRoll;
+    rateCalibrationPitch += ratePitch;
+    rateCalibrationYaw += rateYaw;
   }
 
-  _rateCalibrationRoll /= 2000;
-  _rateCalibrationPitch /= 2000;
-  _rateCalibrationYaw /= 2000;
+  rateCalibrationRoll /= 2000;
+  rateCalibrationPitch /= 2000;
+  rateCalibrationYaw /= 2000;
 }
 
 void HanaMPU6050::loop() {
+  // put your main code here, to run repeatedly:
   mpuSignals();
-  rateRoll -= _rateCalibrationRoll;
-  ratePitch -= _rateCalibrationPitch;
-  rateYaw -= _rateCalibrationYaw;
+  rateRoll -= rateCalibrationRoll;
+  ratePitch -= rateCalibrationPitch;
+  rateYaw -= rateCalibrationYaw;
 
-  KalmanResponse kalmanRoll = _kalmanCalculate(
+  kalmanCalculate(
     kalmanAngleRoll,
     kalmanUncertaintyAngleRoll,
     rateRoll,
     angleRoll
   );
-  kalmanAngleRoll = kalmanRoll.state;
-  kalmanUncertaintyAngleRoll = kalmanRoll.uncertainty;
+  kalmanAngleRoll = kalman1DOutput[0];
+  kalmanUncertaintyAngleRoll = kalman1DOutput[1];
+  
 
-  KalmanResponse kalmanPitch = _kalmanCalculate(
+  kalmanCalculate(
     kalmanAnglePitch,
     kalmanUncertaintyAnglePitch,
     ratePitch,
     anglePitch
   );
-  kalmanAnglePitch = kalmanPitch.state;
-  kalmanUncertaintyAnglePitch = kalmanPitch.uncertainty;
-}
-
-Acceleration HanaMPU6050::getAcceleration() {
-  return {accX, accY, accZ};
+  kalmanAnglePitch = kalman1DOutput[0];
+  kalmanUncertaintyAnglePitch = kalman1DOutput[1];
 }
 
 
-Angle HanaMPU6050::getAngle() {
-  return {angleRoll, anglePitch};
-}
-
-
-Rate HanaMPU6050::getRate() {
-  return {rateRoll, ratePitch, rateYaw};
-}
-
-void HanaMPU6050::mpuSignals() {
+void HanaMPU6050::mpuSignals(void) {
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);
   Wire.write(0x05);
@@ -102,22 +164,23 @@ void HanaMPU6050::mpuSignals() {
   accY = (float) accYLSB / 4096 + ACCY_CORRECTION;
   accZ = (float) accZLSB / 4096 - ACCZ_CORRECTION;
 
-  angleRoll = atan(accY / sqrt(accZ * accZ + accX * accX)) * _rad2deg;
-  anglePitch = - atan(accX / sqrt(accZ * accZ + accY * accY)) * _rad2deg;
+  angleRoll = atan(accY / sqrt(accZ * accZ + accX * accX)) * rad2deg;
+  anglePitch = - atan(accX / sqrt(accZ * accZ + accY * accY)) * rad2deg;
 }
 
-KalmanResponse HanaMPU6050::_kalmanCalculate(
+void HanaMPU6050::kalmanCalculate(
   float kalmanState,
   float kalmanUncertainty,
   float kalmanInput,
   float kalmanMeasurement
 ) {
-  kalmanState = kalmanState + 0.004 * kalmanInput;
-  kalmanUncertainty = kalmanUncertainty + 0.004 * 0.004 * 4 * 4;
+  kalmanState = kalmanState + *Ts * kalmanInput;
+  kalmanUncertainty = kalmanUncertainty + *Ts * *Ts * 4 * 4;
   
   float kalmanGain = kalmanUncertainty * 1 / (1 * kalmanUncertainty + 3 * 3);
   kalmanState = kalmanState + kalmanGain * (kalmanMeasurement - kalmanState);
   kalmanUncertainty = kalmanUncertainty * (1 - kalmanGain);
 
-  return {kalmanState, kalmanUncertainty};
+  kalman1DOutput[0] = kalmanState;
+  kalman1DOutput[1] = kalmanUncertainty;
 }
--- lib/HanaMPU6050/HanaMPU6050.h
+++ lib/HanaMPU6050/HanaMPU6050.h
@@ -4,9 +4,8 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#ifndef HANA_MPU6050_CLOCK
-#define HANA_MPU6050_CLOCK 400000
-#endif
+#define MPU6050_CLOCK_DEFAULT 400000
+#define MPU6050_I2CADDRESS_DEFAULT 0x68
 
 #ifndef ACCX_CORRECTION
   #define ACCX_CORRECTION 0.04
@@ -17,66 +16,100 @@
 #endif
 
 #ifndef ACCZ_CORRECTION
-  #define ACCZ_CORRECTION 0.06
+  #define ACCZ_CORRECTION 0.07
 #endif
 
-float _rad2deg = 1 / (3.142 /* PI */ / 180);
 
-using degree = float;
-
-struct KalmanResponse {
-  degree state;
-  degree uncertainty;
-};
-
-struct Acceleration {
+typedef struct accel_t {
   float x;
   float y;
   float z;
-};
+} mpu6050_accel_t;
 
-struct Angle {
-  degree roll;
-  degree pitch;
-};
+typedef struct rate_t {
+  float roll;
+  float pitch;
+  float yaw;
+} mpu6050_rate_t;
 
-struct Rate {
-  degree roll;
-  degree pitch;
-  degree yaw;
-};
+typedef struct gyro_t {
+  float roll;
+  float pitch;
+  float yaw;
+} mpu6050_gyro_t;
+
+typedef struct angle_t {
+  float roll;
+  float pitch;
+} mpu6050_angle_t;
+
+
+/**
+ * @brief Accelerometer range options
+ *
+ * Allowed values for `setAccelerometerRange`.
+ */
+typedef enum {
+  MPU6050_RANGE_2_G = 0b00,  ///< +/- 2g (default value)
+  MPU6050_RANGE_4_G = 0b01,  ///< +/- 4g
+  MPU6050_RANGE_8_G = 0b10,  ///< +/- 8g
+  MPU6050_RANGE_16_G = 0b11, ///< +/- 16g
+} mpu6050_accel_range_t;
+
+/**
+ * @brief Gyroscope range options
+ *
+ * Allowed values for `setGyroRange`.
+ */
+typedef enum {
+  MPU6050_RANGE_250_DEG,  ///< +/- 250 deg/s (default value)
+  MPU6050_RANGE_500_DEG,  ///< +/- 500 deg/s
+  MPU6050_RANGE_1000_DEG, ///< +/- 1000 deg/s
+  MPU6050_RANGE_2000_DEG, ///< +/- 2000 deg/s
+} mpu6050_gyro_range_t;
+
+
+#define MPU6050_MEASUREMENT_2_G 16384 ///< +/- 2g (default value)
+#define MPU6050_MEASUREMENT_4_G 8192  ///< +/- 4g
+#define MPU6050_MEASUREMENT_8_G 4096  ///< +/- 8g
+#define MPU6050_MEASUREMENT_16_G 2048 ///< +/- 16g
+
+#define MPU6050_MEASUREMENT_250_DEG 131.0  ///< +/- 250 deg/s (default value)
+#define MPU6050_MEASUREMENT_500_DEG 65.5  ///< +/- 500 deg/s
+#define MPU6050_MEASUREMENT_1000_DEG 32.8 ///< +/- 1000 deg/s
+#define MPU6050_MEASUREMENT_2000_DEG 16.4 ///< +/- 2000 deg/s
 
 
 class HanaMPU6050 {
   private:
-    float accX;
-    float accY;
-    float accZ;
+    double *Ts;
+    uint8_t i2c_address;
+    mpu6050_accel_range_t accel_range = MPU6050_RANGE_2_G;
+    mpu6050_gyro_range_t gyro_range = MPU6050_RANGE_250_DEG; 
+    
+    
 
-    float rateRoll, _rateCalibrationRoll;
-    float ratePitch, _rateCalibrationPitch;
-    float rateYaw, _rateCalibrationYaw;
-    float _rateCalibrationNumber;
-
-    float angleRoll;
-    float anglePitch;
-
-    float kalmanAngleRoll = 0;
-    float kalmanAnglePitch = 0;
-
-    float kalmanUncertaintyAngleRoll = 2*2; // 2^4
-    float kalmanUncertaintyAnglePitch = 2*2; // 2^4
-
-    KalmanResponse _kalmanCalculate(
+    void kalmanCalculate(
       float kalmanState, float kalmanUncertainty, float kalmanInput, float kalmanMeasurement);
+    void readAcceleration();
+    void readRate();
     void mpuSignals();
 
   public:
-    void begin();
+    void begin(
+      double *ts,
+      uint8_t i2c_addr = MPU6050_I2CADDRESS_DEFAULT
+    );
+    void setClock(int clock = MPU6050_CLOCK_DEFAULT);
     void loop();
-    Acceleration getAcceleration();
-    Angle getAngle();
-    Rate getRate();
+
+    void setAccelerometerRange(mpu6050_accel_range_t range);
+    void setGyroRange(mpu6050_gyro_range_t range);
+
+    void getRate(mpu6050_rate_t *rate);
+    void getAcceleration(mpu6050_accel_t *acc);
+    void getGyro(mpu6050_gyro_t *gyro);
+    void getAngle(mpu6050_angle_t *angle);
 };
 
 #endif
